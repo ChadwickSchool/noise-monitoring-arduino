697e9bb5ca01de52268773650de828426d4dc976
Sean <sdempsey2023@chadwickschool.org>
2021-10-10 23:17:43 -0700

    feat: creates max and avg for data from sound detector
    resources

--- Arduino/Sound_detector_conneced/Sound_detector_conneced.ino
+++ Arduino/Sound_detector_conneced/Sound_detector_conneced.ino
@@ -1,13 +1,52 @@
+
+  int maxSound = 0;
+  int baseLine = 508;
+
+
+  int relativeSound;
+  int soundInput;
+
+  
+  int sum = 0;
+  int count = 0;
+  double avg = 0;
+  int maxSound = 0;
+
 void setup() {
   
-  Serial.begin(250000); // setup serial
+  Serial.begin(115200); // setup serial
 
 }
 
 void loop() {
-  
-  Serial.println(analogRead(A0));
-  //Serial.println(digitalRead(7));
+
+
+  //get the voltage of the sound detector is sending 
+  soundInput = analogRead(A0);
+
+  //get the voltage of the sound detector is sending 
+  // find the relative 0 and how large the voltage spike above that line
+  // add the relative voltage to a sum.
+  soundInput = analogRead(A0);
+  relativeSound = abs(soundInput - baseLine);
+  sum += relativeSound;
+
+  maxSound = max(maxSound,relativeSound);
+
+  sum += relativeSound;
+
+  if (count == 1000) {
+     avg = (double)sum / (double)count;
+      
+    // send post request here. print is for testing
+    Serial.println(avg);
+    sum = 0;
+    count = 0;
+    avg = 0;
+    maxSound = 0;
+  }
+
+  count++;
 
   delay(1);
 
