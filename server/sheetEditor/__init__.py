34487a346276a92a8a363a7e0afa718bf35d0796
Sean <sdempsey2023@chadwickschool.org>
2021-11-10 14:30:55 -0800

    chore: change script editor to be class. tested it and it works
    resources

--- server/sheetEditor/__init__.py
+++ server/sheetEditor/__init__.py
@@ -8,21 +8,20 @@ from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
 import datetime
 
-def send(average, maximum):
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-    SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE") # 'creds/keys.json'
-    creds = None 
-    creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    SAMPLE_SPREADSHEET_ID = '1Gt-uwLcnEuY6wUygzd6ovZx7KOEWHXH22nz9kZkGUV0'
-    # need to change the sheet and the range here maybe?
-    SAMPLE_RANGE_NAME = "10/27/2021!A3"
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-    date = datetime.datetime.now()
-    val = [["value",average, "value",maximum]]
-    #val = [["value",average],["value",maximum]]
-    request = sheet.values().update(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
-        valueInputOption="USER_ENTERED", body={"values":val}).execute()
-    print("end") 
+class SheetEditor(object):
+    def __init__(self):
+        self.SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        self.SERVICE_ACCOUNT_FILE = 'creds/keysNew.json'
+        self.creds = service_account.Credentials.from_service_account_file(
+        self.SERVICE_ACCOUNT_FILE, scopes=self.SCOPES)
+        self.SAMPLE_SPREADSHEET_ID = '1Gt-uwLcnEuY6wUygzd6ovZx7KOEWHXH22nz9kZkGUV0'
+        self.SAMPLE_RANGE_NAME = "10/27/2021!A3" 
+        self.service = build('sheets', 'v4', credentials=self.creds)
+        self.sheet = self.service.spreadsheets()
+  
+    def send(self,avgVal,maxVal):
+        val = [["value",avgVal, "value",maxVal]]
+        request = self.sheet.values().update(
+            spreadsheetId=self.SAMPLE_SPREADSHEET_ID, range=self.SAMPLE_RANGE_NAME,
+            valueInputOption="USER_ENTERED", body={"values":val}).execute()
+        print("end") 
