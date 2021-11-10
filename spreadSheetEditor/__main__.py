43e45e329829c01aa2347dc2c12ec3f36e0623e3
= <jpalsberg2023@chadwickschool.org>
2021-11-10 11:32:59 -0800

    feat: Changed the send function into a class for more versatility

--- spreadSheetEditor/__main__.py
+++ spreadSheetEditor/__main__.py
@@ -5,27 +5,30 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
-
-def send():
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-    SERVICE_ACCOUNT_FILE = 'creds/keysNew.json'
-    creds = None 
-    creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    SAMPLE_SPREADSHEET_ID = '1Gt-uwLcnEuY6wUygzd6ovZx7KOEWHXH22nz9kZkGUV0'
-    SAMPLE_RANGE_NAME = "10/27/2021!A3"
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-    val = [["value",3],["value",3]]
-    request = sheet.values().update(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
-        valueInputOption="USER_ENTERED", body={"values":val}).execute()
-    print("end") 
+  
+class SheetEditor(object):
+    def __init__(self):
+        self.SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        self.SERVICE_ACCOUNT_FILE = 'creds/keysNew.json'
+        self.creds = service_account.Credentials.from_service_account_file(
+        self.SERVICE_ACCOUNT_FILE, scopes=self.SCOPES)
+        self.SAMPLE_SPREADSHEET_ID = '1Gt-uwLcnEuY6wUygzd6ovZx7KOEWHXH22nz9kZkGUV0'
+        self.SAMPLE_RANGE_NAME = "10/27/2021!A3" 
+        self.service = build('sheets', 'v4', credentials=self.creds)
+        self.sheet = self.service.spreadsheets()
+  
+    def send(self,avgVal,maxVal):
+        val = [["value",avgVal],["value",maxVal]]
+        request = self.sheet.values().update(
+            spreadsheetId=self.SAMPLE_SPREADSHEET_ID, range=self.SAMPLE_RANGE_NAME,
+            valueInputOption="USER_ENTERED", body={"values":val}).execute()
+        print("end") 
 
 def main():
-    send()
-
+   obj = SheetEditor()
+   obj.send(10,50)
+ 
 
 if __name__ == "__main__":
     main()
-    
+     
