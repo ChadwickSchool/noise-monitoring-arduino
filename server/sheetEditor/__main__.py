b9a6167882fd5cc733318eb4e54fca8e7c90c096
Sean <sdempsey2023@chadwickschool.org>
2021-12-03 23:09:44 -0800

    test: tested refectoring of code
    resources

--- server/sheetEditor/__main__.py
+++ server/sheetEditor/__main__.py
@@ -1,10 +1,12 @@
+from os import wait
 from sheetEditor import SheetEditor
 #from sheetEditor import readRange
 
 def main():
     obj = SheetEditor()
     obj.send(10, 9001)
-    #obj.getNumRows("11/12/21")
+    obj.send(11, 9002)
+    #obj.getNumRows("12/03/21")
     #obj.getSheets()
     #obj.autoSheetName()
     #obj.newSheet("lol")
