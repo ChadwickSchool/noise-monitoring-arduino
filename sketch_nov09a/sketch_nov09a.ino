a4c7a748f3d2319fd61aea1526d77357a88b098b
johnpalsberg <54913273+johnpalsberg@users.noreply.github.com>
2022-01-12 10:31:55 -0800

    Create sketch_nov09a.ino
    style: Add comments to Arduino code and clean the code up

--- sketch_nov09a/sketch_nov09a.ino
+++ sketch_nov09a/sketch_nov09a.ino
@@ -2,19 +2,21 @@
 #include <WiFiNINA.h>
 #include <HttpClient.h>
 #include "HTTPClient.h"
-//#include "ESP8266WiFi.h"
 
-char ssid[] = "ChadwickStudent";          //  your network SSID (name)
-char pass[] = "godolphins";   // your network password
-String postData = "value1=4";
-// Initialize the client library
-WiFiClient client;
-IPAddress server(10,11,0,57);
+char ssid[] = "ChadwickStudent";   //  your network SSID (name)
+char pass[] = "godolphins";        // your network password
+String postData = "value1=4";      // test data value
 
-int baseLine = 508; 
 
-int relativeSound;
-int soundInput;
+WiFiClient client;                 // Initialize the client library              
+IPAddress server(10,11,128,175);   //the IP Address of the server the Arduino will connect to 
+
+ 
+int baseLine = 508;  //sound calibration value
+
+
+int relativeSound;   //sound relative to thebaseline
+int soundInput;      // raw sound input value
 
   
 int sum = 0;
@@ -22,9 +24,7 @@ int count = 0;
 double avg = 0;
 int maxSound = 0;
 
-//int[] measureSound() {
- // return [0];
-//}
+// takes the absolute value of input
 double absVal(double val) {
   if (val<0) {
     return -val;
@@ -33,39 +33,20 @@ double absVal(double val) {
   }
   
 }
+
+//resets the Arduino's connection to the server
 void startConnection() {
  client.stop();
  Serial.println("stopped");
  client.connect(server,8000);
  Serial.println("reconnected");
 }
+
+//connects the Arduino to Wifi
 void setup() {
-//  HttpClient http(client);
- // HTTPClient http;
     Serial.begin(9600);
     Serial.println("hi"); 
   
-/*  if (status == WL_CONNECTED) {
-    Serial.println("Already Connected"); 
-  } else {
-    Serial.println("Not Already Connected"); 
-  }
-  
-   
-
-  Serial.println("Attempting to connect to WPA network...");
-  Serial.print("SSID: ");
-  Serial.println(ssid);
-  
-  status = WiFi.begin(ssid, pass);
-  Serial.println(status);
-  if (status != WL_CONNECTED) {
-    Serial.println("Couldn't get a wifi connection");
-    // don't do anything else:
-    while(true);
-  } 
-  else {
-  */
   while (WiFi.status() != WL_CONNECTED) {
       Serial.println("Not Connected to Wifi"); 
     WiFi.begin (ssid,pass); 
@@ -73,60 +54,32 @@ void setup() {
       Serial.println("Bad");
     }
   }     
-  //http://10.11.2.76:8000/update-sheet?avg=67&max=78
-
 
     Serial.println("Connected to wifi");
     Serial.println("\nStarting connection...");
     // if you get a connection, report back via serial:
     if (client.connect(server, 8000)) {
       Serial.println("connected");
-  /*    // Make a HTTP request:
-      //client.println("GET /macros/s/AKfycbwbWCePDN1JfOb7qBkFjAIIqerS5dISDM41FLeW9Vx13pkJIfbBTbDCQAKHrNd5mBVo/exec?value1=4 HTTP/1.1");
-      client.println("GET /update-sheet?avg=117&max=526 HTTP/1.1");
-     // client.println("Host: script.google.com");
- //     client.println("Host: script.google.com");
-  //    client.println("Connection: close");
-      client.println(); 
-  //    Serial.println(client.read());
- 
-  }
- // hwhile
- */
     }
-
 }
 
-void loop() {
-  // client.connect(server,8000);
-  //startConnection();
+//measures the sound input to the Arduino, and sends the data to the server
+void loop() { 
   // put your main code here, to run repeatedly:
-   /*  if (client.available()) {
-      Serial.print(client.read());
-     } 
-       delay(100);
-       */
    soundInput = analogRead(A0); // get the voltage of the sound detector is sending 
   relativeSound = abs(soundInput - baseLine); // find the relative 0 and how large the voltage spike above that line
   sum += relativeSound; // add the relative voltage to a sum
- // Serial.println(sum);
-  if (relativeSound<0) {
-   // Serial.println("Negative Sound! SOUND the Alarm!");
-  } 
   maxSound = max(maxSound,relativeSound); // find the max sound with the max fucntion
   if (count >= 100) { // if it has looped 1000 times
     startConnection();
    if (sum <0) {
-  //  Serial.println("WAHHH!");
    }
-    avg = absVal((double)sum / (double)count); // caculute the avrage
-   client.println("GET /update-sheet?avg="+ (String)avg + "&max=" + (String)maxSound + " HTTP/1.1");
+    avg = absVal((double)sum / (double)count); // caculute the average
+   client.println("GET /update-sheet?avg="+ (String)avg + "&max=" + (String)maxSound + " HTTP/1.1"); //send data to the server
  if (!client.connected()) {
-  Serial.println("bad, Very Bad!");
+  Serial.println("bad, Very Bad!"); //very bad to not be connected during this stage
  }
- //  client.println("Connection: Keep-Alive"); 
      Serial.println("sent");
-     //client.println("Connection: close");
    client.println(); 
     sum = 0; // reset values back down to 0
     count = 0;
@@ -134,13 +87,5 @@ void loop() {
     maxSound = 0;
   }
   count++;
-  delay(1);
-           // Make a HTTP request:
-      //client.println("GET /macros/s/AKfycbwbWCePDN1JfOb7qBkFjAIIqerS5dISDM41FLeW9Vx13pkJIfbBTbDCQAKHrNd5mBVo/exec?value1=4 HTTP/1.1");
-
-     // client.println("Host: script.google.com");
- //     client.println("Host: script.google.com");
-  //    client.println("Connection: close");
-      
-      
+  delay(1); //small delay for timing
 }
